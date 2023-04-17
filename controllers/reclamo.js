ff24aaf54eff7f5be3dc7f24c5344d2135719d0c
melinamais <118691588+melinamais@users.noreply.github.com>
2023-04-17 15:22:37 -0300

    Update reclamo.js

--- controllers/reclamo.js
+++ controllers/reclamo.js
@@ -1,69 +1,62 @@
 $(document).ready(() => {
-    $(".boton-enviar").click((e) => {
+    $(".boton-enviar").click(() => {
         let formulario = new FormValidator('formulario-reclamo',[
             {
                 name : "nombre",
-                display : "nombre",
                 rules : "required|min_length[5]"
             },
             {
                 name : "email",
-                display : "@email",
                 rules : "required|valid_email"
             },
             {
                 name : "telefono",
-                display : "telefono",
                 rules : "required|min_length[11]"
             },
             {
                 name : "reclamo",
-                display : "reclamo",
                 rules : "required|min_length[20]"
             }
         ],function(errores, evento){
             if(errores.length > 0){
+                let leer_primer_error = errores[0].message;  //Mensaje en ingles
+                //Pasos para mostrar el mensaje en español
+                let nombre_input = leer_primer_error.split(" ")[1];
+
                 let mensaje = '';
-                errores.forEach((campo) => {
-                    let nombre = campo.message.split(" ")[1];
-                    if(campo.message.split(" ")[7]!=undefined){
-                        mensaje += "😐 Error campo "+nombre+" debe tener por lo menos "+campo.message.split(" ")[7]+" caracteres";
-                    }else{
-                        switch(nombre){
-                            case "nombre":
-                                mensaje += "😐 Error campo "+nombre+" es obligatorio";
-                            break;
-                            case "@email":
-                                mensaje += "😐 Error campo "+nombre+" es obligatorio";
-                            break;
-                            case "telefono":
-                                mensaje += "😐 Error campo "+nombre+" es obligatorio";
-                            break;
-                            case "reclamo":
-                                mensaje += "😐 Error campo "+nombre+" es obligatorio";
-                            break;
-                        }
-                    }
-                })
+                if(leer_primer_error.split(" ")[7]!=undefined){
+                    mensaje = "😐 Error campo "+nombre_input+" debe tener por lo menos "+leer_primer_error.split(" ")[7]+" caracteres";
+                }else{
+                    mensaje = "😐 Error campo "+nombre_input+" es obligatorio";
+                }
                 $(".mensaje-error").css({"display":"block"});
                 document.querySelector('.mensaje-error').innerText = mensaje;
             }else{
                 $(".mensaje-error").hide();
                 evento.preventDefault();
-                $(".mensaje-confirmacion").css({"display" : "block"});
-                $("#reclamos").css({"opacity" : "0.3"});
+                $(".mensaje-confirmacion").css({
+                    "display" : "block"
+                });
+                $("#reclamos").css({
+                    "opacity" : "0.3"
+                });
             }
         });
     })
-  
+    
+    //Ocultar mensaje de confirmacion del reclamo (Modal)
     $(".boton-cerrar").click(() => {
         $(".mensaje-confirmacion").hide();
-        $("#reclamos").css({"opacity" : "1"})
+        $("#reclamos").css({
+            "opacity" : "1"
+        })
         document.querySelector("form").reset();
     })
     $(".boton-aceptar").click(() => {
         $(".mensaje-confirmacion").hide();
-        $("#reclamos").css({"opacity" : "1"})
+        $("#reclamos").css({
+            "opacity" : "1"
+        })
         document.querySelector("form").reset();
     })
 });
